96bd89f688dffd2622debd2aae95f04a9cd01838
Andrew Hayhurst <hayhurst.andrew@gmail.com>
2023-02-26 22:37:22 -0800

    Add function to return all possible ship coordinates for a given length

--- src/gameboard.js
+++ src/gameboard.js
@@ -14,7 +14,10 @@ export default function gameBoardFactory() {
   const placeShip = (coords) => {
     if (outOfRange(coords)) throw new Error('Ships cannot be placed off the board');
     if (isOccupied(coords)) throw new Error('Ships cannot be on top of ships');
-    if (!inLine(coords)) throw new Error('Ships must be placed vertically or horizontally in an unbroken line');
+    if (!inLine(coords))
+      throw new Error(
+        'Ships must be placed vertically or horizontally in an unbroken line'
+      );
 
     const newShip = shipFactory(coords.length);
     coords.forEach((coord) => (squares[coord[0]][coord[1]].ship = newShip));
@@ -27,31 +30,35 @@ export default function gameBoardFactory() {
       if (squares[coord[0]][coord[1]].ship) return true;
     }
     return false;
-  }
+  };
 
   const outOfRange = (coords) => {
-    return coords.flat().some(coord => coord < 0 || coord > 9);
-  }
+    return coords.flat().some((coord) => coord < 0 || coord > 9);
+  };
 
   const inLine = (coords) => {
-    if(coords.every((coord, index) => coord[0] === coords[0][0])) {
+    if (coords.every((coord, index) => coord[0] === coords[0][0])) {
       // horizontal
       coords.sort((a, b) => a[1] - b[1]);
-      return coords.every((coord, index) => index === 0 || Math.abs(coord[1] - coords[index - 1][1]) === 1);
+      return coords.every(
+        (coord, index) => index === 0 || Math.abs(coord[1] - coords[index - 1][1]) === 1
+      );
     } else if (coords.every((coord, index) => coord[1] === coords[0][1])) {
       // vertical
       coords.sort((a, b) => a[0] - b[0]);
-      return coords.every((coord, index) => index === 0 || Math.abs(coord[0] - coords[index - 1][0]) === 1);
+      return coords.every(
+        (coord, index) => index === 0 || Math.abs(coord[0] - coords[index - 1][0]) === 1
+      );
     }
     return false;
-  }
+  };
 
   function receiveAttack(coords) {
     const square = squares[coords[0]][coords[1]];
     if (square.attacked) throw new Error('this square has already been attacked');
-    if(square.ship) {
+    if (square.ship) {
       square.ship.hit();
-      if(square.ship.isSunk()) shipsSunk++;
+      if (square.ship.isSunk()) shipsSunk++;
     }
     square.attacked = true;
   }
@@ -60,7 +67,67 @@ export default function gameBoardFactory() {
     return totalShips === shipsSunk;
   }
 
-  return { squares, placeShip, receiveAttack, gameOver };
+  function findSets(length) {
+    if (length === 1) return emptySquares();
+    let sets = [];
+    for (let i = 0; i < squares.length; i++) {
+      const horizontal = [];
+      const vertical = [];
+      for (let j = 0; j < squares.length; j++) {
+        horizontal.push([j, i]);
+        vertical.push([i, j]);
+      }
+      sets = [
+        ...sets,
+        ...findSetsFromRow(horizontal, length),
+        ...findSetsFromRow(vertical, length),
+      ];
+    }
+    return sets;
+  }
+
+  function findSetsFromRow(row, length) {
+    let lft = 0;
+    let rt = 1;
+    const sets = [];
+
+    while (rt < row.length) {
+      if (squares[row[lft][0]][row[lft][1]].ship) {
+        lft = rt;
+        rt = rt + 1;
+      } else if (squares[row[rt][0]][row[rt][1]].ship) {
+        lft = rt + 1;
+        rt = rt + 2;
+      } else {
+        if (rt - lft + 1 === length) {
+          const set = [];
+          for (let j = lft; j <= rt; j++) {
+            set.push(row[j]);
+          }
+          sets.push(set);
+          lft++;
+          rt++;
+        } else {
+          rt++;
+        }
+      }
+    }
+    return sets;
+  }
+
+  function emptySquares() {
+    const set =  [];
+    for (let i = 0; i < squares.length; i++) {
+      for (let j = 0; j < squares.length; j++) {
+        if (!squares[i][j].ship) {
+          set.push([i, j]);
+        }
+      }
+    }
+    return set;
+  }
+
+  return { squares, placeShip, receiveAttack, gameOver, findSets };
 }
 
 function squareFactory() {
--- test/gameboard.test.js
+++ test/gameboard.test.js
@@ -1,7 +1,7 @@
 import gameBoardFactory from '../src/gameBoard';
 
 let gameBoard;
-beforeEach(() => gameBoard = gameBoardFactory());
+beforeEach(() => (gameBoard = gameBoardFactory()));
 
 describe('initialization', () => {
   test('initializes with 100 Square objects', () => {
@@ -9,7 +9,7 @@ describe('initialization', () => {
     expect(gameBoard.squares[0][4]).toHaveProperty('ship');
     expect(gameBoard.squares[1][10]).toBeUndefined();
   });
-})
+});
 
 describe('placeShip', () => {
   test('places ship given coordinates', () => {
@@ -22,7 +22,7 @@ describe('placeShip', () => {
     const ship = gameBoard.squares[0][3].ship;
     expect(gameBoard.squares[0][4].ship).toBe(ship);
   });
-  
+
   test('will not place a ship onto another ship', () => {
     gameBoard.placeShip([
       [1, 2],
@@ -36,7 +36,7 @@ describe('placeShip', () => {
       ])
     ).toThrow('Ships cannot be on top of ships');
   });
-  
+
   test('will not place a ship off the board', () => {
     expect(() =>
       gameBoard.placeShip([
@@ -45,7 +45,7 @@ describe('placeShip', () => {
       ])
     ).toThrow('Ships cannot be placed off the board');
   });
-  
+
   describe('ships must be placed vertically or horizontally in an unbroken line', () => {
     const msg = 'Ships must be placed vertically or horizontally in an unbroken line';
     test('ships must be placed vertically or horizontally', () => {
@@ -56,7 +56,7 @@ describe('placeShip', () => {
         ])
       ).toThrow(msg);
     });
-  
+
     test('horizontal ships cannot have gaps', () => {
       expect(() =>
         gameBoard.placeShip([
@@ -67,32 +67,54 @@ describe('placeShip', () => {
         ])
       ).toThrow(msg);
     });
-  
+
     test('vertical ships also cannot have gaps', () => {
-      expect(() => gameBoard.placeShip([[6, 3], [6, 4], [6, 6]]).toThrow(msg));
-    })
-  
+      expect(() =>
+        gameBoard
+          .placeShip([
+            [6, 3],
+            [6, 4],
+            [6, 6],
+          ])
+          .toThrow(msg)
+      );
+    });
+
     test('out of order horizontal coordinates are ok', () => {
-      gameBoard.placeShip([[2, 2], [2, 5], [2, 4], [2, 3]]);
+      gameBoard.placeShip([
+        [2, 2],
+        [2, 5],
+        [2, 4],
+        [2, 3],
+      ]);
       const ship = gameBoard.squares[2][5].ship;
       expect(gameBoard.squares[2][3].ship).toBe(ship);
-    })
-  
+    });
+
     test('out of order vertical coordinates are ok', () => {
-      gameBoard.placeShip([[9, 8], [9, 3], [9, 7], [9, 4], [9, 5], [9, 6]]);
+      gameBoard.placeShip([
+        [9, 8],
+        [9, 3],
+        [9, 7],
+        [9, 4],
+        [9, 5],
+        [9, 6],
+      ]);
       const ship = gameBoard.squares[9][3].ship;
       expect(gameBoard.squares[9][5].ship).toBe(ship);
-    })
+    });
   });
-})
+});
 
 describe('receive attack', () => {
   test('cannot attack the same square twice', () => {
     gameBoard.receiveAttack([2, 3]);
     gameBoard.receiveAttack([3, 4]);
     gameBoard.receiveAttack([8, 9]);
-    expect(() => gameBoard.receiveAttack([2, 3])).toThrow('this square has already been attacked');
-  })
+    expect(() => gameBoard.receiveAttack([2, 3])).toThrow(
+      'this square has already been attacked'
+    );
+  });
 
   test('can sink a ship', () => {
     gameBoard.placeShip([
@@ -107,9 +129,9 @@ describe('receive attack', () => {
     gameBoard.receiveAttack([3, 2]);
     expect(ship.isSunk()).toBe(true);
   });
-})
+});
 
-describe ('gameOver', () => {
+describe('gameOver', () => {
   test('reports whether all ships have sunk', () => {
     gameBoard.placeShip([
       [1, 2],
@@ -120,7 +142,7 @@ describe ('gameOver', () => {
       [6, 6],
       [6, 9],
       [6, 8],
-      [6, 7]
+      [6, 7],
     ]);
     gameBoard.receiveAttack([1, 2]);
     gameBoard.receiveAttack([2, 2]);
@@ -131,5 +153,279 @@ describe ('gameOver', () => {
     gameBoard.receiveAttack([6, 8]);
     gameBoard.receiveAttack([6, 7]);
     expect(gameBoard.gameOver()).toBe(true);
+  });
+});
+
+describe('randomly placing  ships', () => {
+  test('findSets returns an array of all valid coordinates for ship of length n', () => {
+    gameBoard.placeShip([
+      [0, 5],
+      [1, 5],
+    ]);
+    gameBoard.placeShip([
+      [2, 3],
+      [2, 4],
+      [2, 5],
+      [2, 6],
+      [2, 7],
+    ]);
+    gameBoard.placeShip([
+      [3, 0],
+      [3, 1],
+      [3, 2],
+    ]);
+    gameBoard.placeShip([
+      [4, 7],
+      [4, 8],
+      [4, 9],
+    ]);
+    gameBoard.placeShip([
+      [4, 2],
+      [5, 2],
+      [6, 2],
+      [7, 2],
+      [8, 2],
+    ]);
+    gameBoard.placeShip([
+      [6, 0],
+      [7, 0],
+    ]);
+    gameBoard.placeShip([
+      [5, 4],
+      [5, 5],
+    ]);
+    gameBoard.placeShip([
+      [5, 6],
+      [6, 6],
+      [7, 6],
+    ]);
+    gameBoard.placeShip([
+      [6, 8],
+      [6, 9],
+    ]);
+    gameBoard.placeShip([
+      [9, 3],
+      [9, 4],
+    ]);
+
+    const expected = [
+      [
+        [4, 1],
+        [5, 1],
+        [6, 1],
+        [7, 1],
+        [8, 1],
+      ],
+      [
+        [5, 1],
+        [6, 1],
+        [7, 1],
+        [8, 1],
+        [9, 1],
+      ],
+      [
+        [3, 3],
+        [4, 3],
+        [5, 3],
+        [6, 3],
+        [7, 3],
+      ],
+      [
+        [4, 3],
+        [5, 3],
+        [6, 3],
+        [7, 3],
+        [8, 3],
+      ],
+      [
+        [5, 7],
+        [6, 7],
+        [7, 7],
+        [8, 7],
+        [9, 7],
+      ],
+      [
+        [0, 0],
+        [0, 1],
+        [0, 2],
+        [0, 3],
+        [0, 4],
+      ],
+      [
+        [1, 0],
+        [1, 1],
+        [1, 2],
+        [1, 3],
+        [1, 4],
+      ],
+      [
+        [3, 3],
+        [3, 4],
+        [3, 5],
+        [3, 6],
+        [3, 7],
+      ],
+      [
+        [3, 4],
+        [3, 5],
+        [3, 6],
+        [3, 7],
+        [3, 8],
+      ],
+      [
+        [3, 5],
+        [3, 6],
+        [3, 7],
+        [3, 8],
+        [3, 9],
+      ],
+      [
+        [8, 3],
+        [8, 4],
+        [8, 5],
+        [8, 6],
+        [8, 7],
+      ],
+      [
+        [8, 4],
+        [8, 5],
+        [8, 6],
+        [8, 7],
+        [8, 8],
+      ],
+      [
+        [8, 5],
+        [8, 6],
+        [8, 7],
+        [8, 8],
+        [8, 9],
+      ],
+      [
+        [9, 5],
+        [9, 6],
+        [9, 7],
+        [9, 8],
+        [9, 9],
+      ],
+    ];
+
+    expect(gameBoard.findSets(5).length).toBe(expected.length);
+    expect(gameBoard.findSets(5)).toEqual(expect.arrayContaining(expected));
+  });
+
+  test('findSets returns an array of all valid coordinates for ship of length n', () => {
+    gameBoard.placeShip([
+      [4, 9],
+      [5, 9],
+    ]);
+    gameBoard.placeShip([
+      [4, 6],
+      [5, 6],
+      [6, 6],
+    ]);
+    gameBoard.placeShip([
+      [6, 4],
+      [7, 4],
+      [8, 4],
+      [9, 4],
+    ]);
+    gameBoard.placeShip([
+      [3, 3],
+      [4, 3],
+      [5, 3],
+      [6, 3],
+    ]);
+    gameBoard.placeShip([
+      [3, 1],
+      [4, 1],
+      [5, 1],
+      [6, 1],
+    ]);
+    gameBoard.placeShip([
+      [4, 0],
+      [5, 0],
+    ]);
+    gameBoard.placeShip([
+      [0, 3],
+      [0, 4],
+      [0, 5],
+      [0, 6],
+    ]);
+    gameBoard.placeShip([
+      [1, 2],
+      [1, 3],
+    ]);
+    gameBoard.placeShip([
+      [1, 7],
+      [1, 8],
+    ]);
+    gameBoard.placeShip([
+      [2, 2],
+      [2, 3],
+    ]);
+    gameBoard.placeShip([
+      [3, 5],
+      [3, 6],
+      [3, 7],
+      [3, 8],
+    ]);
+    gameBoard.placeShip([
+      [7, 5],
+      [7, 6],
+      [7, 7],
+      [7, 8],
+    ]);
+    gameBoard.placeShip([
+      [8, 2],
+      [8, 3],
+    ]);
+    gameBoard.placeShip([[9, 6]]);
+
+    const expected = [
+      [
+        [1, 4],
+        [2, 4],
+        [3, 4],
+        [4, 4],
+        [5, 4],
+      ],
+      [
+        [3, 2],
+        [4, 2],
+        [5, 2],
+        [6, 2],
+        [7, 2],
+      ],
+      [
+        [2, 4],
+        [2, 5],
+        [2, 6],
+        [2, 7],
+        [2, 8],
+      ],
+      [
+        [2, 5],
+        [2, 6],
+        [2, 7],
+        [2, 8],
+        [2, 9],
+      ],
+      [
+        [8, 5],
+        [8, 6],
+        [8, 7],
+        [8, 8],
+        [8, 9],
+      ],
+    ];
+
+    expect(gameBoard.findSets(5).length).toBe(expected.length);
+    expect(gameBoard.findSets(5)).toEqual(expect.arrayContaining(expected));
+  });
+
+  test('findSets returns an array of all valid coordinates for ship of length n', () => {
+    gameBoard.placeShip([[0, 9]]);
+    expect(gameBoard.findSets(2).length).toBe(178);
+    expect(gameBoard.findSets(1).length).toBe(99);
   })
-})
+});
