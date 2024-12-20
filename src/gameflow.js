24facbf7d76c5c021f83c459e5a9d5618b0d4f42
Andrew Hayhurst <hayhurst.andrew@gmail.com>
2024-12-19 19:41:11 -0800

    Fix bug caused by reset not removing finishSetup and startGame event listeners

--- src/gameflow.js
+++ src/gameflow.js
@@ -195,4 +195,6 @@ function reset() {
   attackCount = 0;
   DOMBoard1.unlistenForAttack();
   DOMBoard2.unlistenForAttack();
+  setBoardButton.removeEventListener('click', finishSetup, { once: true });
+  setBoardButton.removeEventListener('click', startGame, { once: true });
 }
